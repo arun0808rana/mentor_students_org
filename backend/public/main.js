0da94732661af0760c28782254dc624f77c1eb6d
arun0808rana <arun0808rana@gmail.com>
2023-08-31 10:08:02 +0530

    feat:screen recorder

--- backend/public/main.js
+++ backend/public/main.js
@@ -2,3 +2,15 @@ const email = document.querySelector('.email');
 const password = document.querySelector('.password');
 const submitBtn = document.querySelector('.submit');
 
+let camera_button = document.querySelector("#start-camera");
+let video = document.querySelector("#video");
+let start_button = document.querySelector("#start-record");
+let stop_button = document.querySelector("#stop-record");
+let download_link = document.querySelector("#download-video");
+const stopCameraBtn = document.querySelector(".stop-camera");
+const start_screen_recording_btn = document.querySelector(
+  ".start-screen-recording-btn"
+);
+const downloadScreenRecordingBtn = document.querySelector(
+  ".download-screen-recording"
+);
---
+++ backend/public/screen_recorder.js
@@ -0,0 +1,46 @@
+ // screen recording logic start
+ start_screen_recording_btn.addEventListener("click", async function () {
+    let chunks = [];
+
+    let stream = await navigator.mediaDevices.getDisplayMedia({
+      video: true,
+      audio: true,
+    });
+
+    //needed for better browser support
+    const mime = MediaRecorder.isTypeSupported("video/webm; codecs=vp9")
+      ? "video/webm; codecs=vp9"
+      : "video/webm";
+    let mediaRecorder = new MediaRecorder(stream, {
+      mimeType: mime,
+    });
+
+    mediaRecorder.addEventListener("dataavailable", function (e) {
+      chunks.push(e.data);
+    });
+
+    mediaRecorder.addEventListener("stop", function () {
+      let blob = new Blob(chunks, {
+        type: chunks[0].type,
+      });
+
+      video.src = URL.createObjectURL(blob);
+    });
+
+    //we have to start the recorder manually
+    mediaRecorder.start();
+
+    mediaRecorder.addEventListener("stop", function () {
+      let blob = new Blob(chunks, {
+        type: chunks[0].type,
+      });
+      let url = URL.createObjectURL(blob);
+
+      video.src = url;
+
+      downloadScreenRecordingBtn.href = url;
+      downloadScreenRecordingBtn.download = "video.webm";
+    });
+  });
+
+  // screen recording logic end
